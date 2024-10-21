640e678e39ff51d042404cf01e6760d962b6e06e
Hélio do Nascimento Silva <hedonasi.1979@gmail.com>
2024-10-21 13:09:53 -0300

    18:28

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 import pyautogui
-import time
+from time import sleep
 
-print(pyautogui.position())
-print(pyautogui.size())
 
-time.sleep(10)
-pyautogui.click(x=343, y=193)
+def auto():
+    pyautogui.PAUSE = 0.5
+
+    print(pyautogui.position())
+    print(pyautogui.size())
+
+    sleep(10)
+    pyautogui.moveTo(x=333, y=173, duration=1)
+
+    sleep(1)
+    pyautogui.moveTo(x=273, y=339, duration=1)
+
+    sleep(1)
+    pyautogui.moveTo(x=645, y=280, duration=1)
+
+    sleep(1)
+    pyautogui.click(x=645, y=280)
+
+
+auto()
